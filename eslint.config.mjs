ffe7d100a57e8c99c5513947f5c27d14fb570b3a
Hamdani <hamdani@synergizetech.dev>
2024-12-29 22:59:07 +0800

    setting up eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,115 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import importPlugin from 'eslint-plugin-import'
+import tseslint from 'typescript-eslint'
+import tsEslintParser from '@typescript-eslint/parser'
+
+export default tseslint.config(
+  { ignores: ['dist', 'node_modules', '.react-router'] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      jsxA11y.flatConfigs.recommended,
+      importPlugin.flatConfigs.recommended
+    ],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser
+    },
+    plugins: {
+      'react-hooks': reactHooks
+    },
+
+    languageOptions: {
+      parser: tsEslintParser
+    },
+    settings: {
+      'import/internal-regex': '^@/',
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json'
+        },
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx']
+        }
+      }
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true
+        }
+      ],
+      'jsx-a11y/no-static-element-interactions': 'off',
+      'jsx-a11y/click-events-have-key-events': 'off',
+      'no-console': 'error',
+      'no-empty-pattern': 'off',
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: [
+            {
+              group: [
+                '../*',
+                '../',
+                '..',
+                '@/apis/*/*',
+                '@/configs/*/*',
+                '@/components/*/*',
+                '@/contexts/*/*',
+                '@/factories/*/*',
+                '@/features/*/*',
+                '@/hooks/*/*',
+                '@/layouts/*/*',
+                '@/schemas/*/*',
+                '@/types/*/*',
+                '@/utils/*/**'
+              ]
+            }
+          ]
+        }
+      ],
+      'linebreak-style': ['error', 'unix'],
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+            'object'
+          ],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true }
+        }
+      ],
+      'import/default': 'off',
+      'import/no-named-as-default-member': 'off',
+      'import/no-named-as-default': 'off',
+      'import/no-unresolved': [
+        'error',
+        {
+          ignore: [
+            '\\.(png|svg|jpg|jpeg|gif|webp|ico|bmp|tiff|mp4|mp3|woff|woff2|eot|ttf|otf)$'
+          ]
+        }
+      ]
+    }
+  }
+)
